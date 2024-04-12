0c7295e49c15f5fa45b4783ab8fbc53dc481ca1d
Nikita Anishenco <tanishenco91@gmail.com>
2024-04-12 11:55:41 +0300

    Добавьте файлы проекта.

---
+++ DevTask/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DevTask
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
