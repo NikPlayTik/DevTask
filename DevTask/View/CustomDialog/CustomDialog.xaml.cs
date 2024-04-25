5257b84e0468093e473c30d9e6b37f7671f5cfde
Nikita Anishenco <tanishenco91@gmail.com>
2024-04-25 22:36:51 +0300

    Добавлено собственное диалоговое окно на разные случаи замена MessageBox.
    Добавлены регулярные выражения для эл.почты, пароля.

---
+++ DevTask/View/CustomDialog/CustomDialog.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DevTask.View.CustomDialog
+{
+    public partial class CustomDialog : Window
+    {
+        public CustomDialog(string message, Brush messageColor)
+        {
+            InitializeComponent();
+            MessageLabel.Content = message;
+            MessageLabel.Foreground = messageColor;
+        }
+        public CustomDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        public static void Show(string message, Brush messageColor)
+        {
+            var dialog = new CustomDialog(message, messageColor);
+            dialog.ShowDialog();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
---
+++ DevTask/View/Registration/RegistrationWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+//using DevTask.View.CustomDialog;
+
+namespace DevTask.View.Registration
+{
+    public partial class RegistrationWindow : Window
+    {
+        private const string FirebaseAppUri = "https://devtaskdb-default-rtdb.europe-west1.firebasedatabase.app/";
+        private FirebaseClient _client;
+
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+            _client = new FirebaseClient(FirebaseAppUri);
+        }
+
+        private async void RegisterUser(object sender, RoutedEventArgs e)
+        {
+            var username = UsernameTextBox.Text;
+            var email = EmailTextBox.Text;
+            var password = PasswordTextBox.Password;
+
+            // Валидация электронной почты
+            var emailRegex = new Regex(@"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$");
+            if (!emailRegex.IsMatch(email))
+            {
+                // Обработка неверного адреса электронной почты
+                CustomDialog.CustomDialog.Show("Неверный адрес электронной почты!", Brushes.Red);
+                return;
+            }
+
+            // Валидация пароля: как минимум две цифры и символы
+            var passwordRegex = new Regex(@"^(?=.*\d{2,})(?=.*[a-zA-Z]).+$");
+            if (!passwordRegex.IsMatch(password))
+            {
+                // Обработка неверного пароля
+                CustomDialog.CustomDialog.Show("Пароль должен содержать как минимум две цифры и символы!", Brushes.Red);
+                return;
+            }
+
+            var user = new 
+            { 
+                Username = username, 
+                Email = email, 
+                Password = password 
+            };
+
+            await _client
+                .Child("Users")
+                .PostAsync(user);
+
+            // Сообщение об успешной регистрации
+            CustomDialog.CustomDialog.Show("Вы успешно зарегистрировались!", Brushes.Green);
+        }
+    }
+}
